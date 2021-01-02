d1241d4f4b5a3b3a6f5c9330f11a742f47eb0c74
Yuchen Zhang <zyc.gary@gmail.com>
2021-01-02 23:49:42 +1100

    Wechat Oauth

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthorizationsController;
 use App\Http\Controllers\Api\VerificationCodesController;
+use App\Http\Controllers\UsersController;
 use App\Http\Middleware\AcceptHeader;
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +22,13 @@ Route::prefix('v1')
     ->name('api.v1.')
     ->middleware([AcceptHeader::class])
     ->group(function () {
-        Route::post('verificationCodes', [VerificationCodesController::class, 'store'])->name('verificationCodes.store');
+        Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
+            ->name('verificationCodes.store');
+        // 用户注册
+        Route::post('users', [UsersController::class, 'store'])
+            ->name('users.store');
+        // 第三方登录
+        Route::post('socials/{social_type}/authorizations', [AuthorizationsController::class, 'socialStore'])
+            ->where('social_type', 'wechat')
+            ->name('socials.authorizations.store');
     });
